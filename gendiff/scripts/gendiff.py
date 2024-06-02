418c90082ed9ff272540c14a159d6a8318f95d57
usman sakhbeev <sahbeyev@gmail.com>
2024-06-02 14:25:10 +0300

    add gendiff

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument("first file")
+    parser.add_argument("second file")
+
+    args = parser.parse_args()
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
