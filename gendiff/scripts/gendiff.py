729c3c497d10155d0e9451369d3cc586b4dcc5be
UsmanSakhbeev <sahbeyev@gmail.com>
2024-06-30 12:24:38 +0300

    update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -12,7 +12,7 @@ def main():
 
     args = parser.parse_args()
 
-    diff = generate_diff(args.first_file, args.second_file)    
+    return generate_diff(args.first_file, args.second_file)
 
 
 def get_text(file_path: str):
