17a1368fc417b573ddf99058a01190d0aefab02c
UsmanSakhbeev <sahbeyev@gmail.com>
2024-07-17 14:00:18 +0300

    lint updates

--- tests/test_json_merge.py
+++ tests/test_json_merge.py
@@ -1,4 +1,3 @@
-import pytest
 from gendiff.scripts.gendiff import generate_diff
 
 
@@ -10,35 +9,33 @@ JSON_RESULT = "tests/fixtures/diff.json"
 PLAIN_RESULT = "tests/fixtures/plain_diff"
 YAML_RESULT = "tests/fixtures/json.json"
 
-def test_json_merge():    
+
+def test_json_merge():
 
     with open(JSON_RESULT) as f:
-        expected_result = f.read().strip()    
+        expected_result = f.read().strip()
     assert generate_diff(JSON1, JSON2, "stylish").strip() == expected_result
 
     with open(PLAIN_RESULT) as f:
-        expected_result = f.read().strip()    
+        expected_result = f.read().strip()
     assert generate_diff(JSON1, JSON2, "plain").strip() == expected_result
 
     with open(YAML_RESULT) as f:
-        expected_result = f.read().strip()    
+        expected_result = f.read().strip()
     assert generate_diff(JSON1, JSON2, "json").strip() == expected_result
-
 
 
 def test_yml_merge():
 
     with open(JSON_RESULT) as f:
-        expected_result = f.read().strip()    
+        expected_result = f.read().strip()
     assert generate_diff(YML1, YML2, "stylish").strip() == expected_result
 
     with open(PLAIN_RESULT) as f:
-        expected_result = f.read().strip()    
+        expected_result = f.read().strip()
     assert generate_diff(YML1, YML2, "plain").strip() == expected_result
 
     with open(YAML_RESULT) as f:
-        expected_result = f.read().strip()    
+        expected_result = f.read().strip()
     assert generate_diff(YML1, YML2, "json").strip() == expected_result
-
-
     
