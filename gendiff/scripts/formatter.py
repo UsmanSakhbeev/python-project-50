bf6fb494db048388957b786bf4594fb51ac3cb00
UsmanSakhbeev <sahbeyev@gmail.com>
2024-07-21 23:32:53 +0300

    update formatter

--- gendiff/scripts/formatter.py
+++ gendiff/scripts/formatter.py
@@ -1,38 +1,51 @@
 def stylish_formatter(value):
-    replacer = " "
 
-    def build(current_value, depth):
+    def build(current_value, depth, replacer=" "):
         if not isinstance(current_value, dict):
             return format_exception_check(current_value)
 
-        current_intend = replacer * depth
-        child_intend_size = depth + 4
+        indent = replacer * depth
+        child_depth = depth + 4
         result = ["{"]
 
         for key, val in current_value.items():
-            current_string = ""
+            string = ""
             if isinstance(val, dict):
-                if val.get("type") == "chained":
-                    current_string = f"{current_intend}    {key}: {build(val['value'], child_intend_size)}"
-                elif val.get("type") == "added":
-                    current_string = f"{current_intend}  + {key}: {build(val['value'], child_intend_size)}"
-                elif val.get("type") == "deleted":
-                    current_string = f"{current_intend}  - {key}: {build(val['value'], child_intend_size)}"
-                elif val.get("type") == "unchanged":
-                    current_string = f"{current_intend}    {key}: {build(val['value'], child_intend_size)}"
-                elif val.get("type") == "changed":
-                    current_string = f"{current_intend}  - {key}: {build(val['old_value'], child_intend_size)}"
-                    result.append(current_string)
-                    current_string = f"{current_intend}  + {key}: {build(val['new_value'], child_intend_size)}"
-                else:
-                    current_string = f"{current_intend}    {key}: {build(val, child_intend_size)}"
-                result.append(current_string)
+                match val.get("type"):
+                    case "chained":
+                        string = (
+                            f"{indent}    {key}: {build(val['value'], child_depth)}"
+                        )
+                    case "added":
+                        string = (
+                            f"{indent}  + {key}: {build(val['value'], child_depth)}"
+                        )
+                    case "deleted":
+                        string = (
+                            f"{indent}  - {key}: {build(val['value'], child_depth)}"
+                        )
+                    case "unchanged":
+                        string = (
+                            f"{indent}    {key}: {build(val['value'], child_depth)}"
+                        )
+                    case "changed":
+                        string = (
+                            f"{indent}  - {key}: {build(val['old_value'], child_depth)}"
+                        )
+                        result.append(string)
+                        string = (
+                            f"{indent}  + {key}: {build(val['new_value'], child_depth)}"
+                        )
+                    case _:
+                        string = f"{indent}    {key}: {build(val, child_depth)}"
+                result.append(string)
             else:
-                current_string = (f"{current_intend}    {key}: {build(val, child_intend_size)}")
-                result.append(current_string)
+                string = f"{indent}    {key}: {build(val, child_depth)}"
+                result.append(string)
 
-        result.append(current_intend + "}")
+        result.append(indent + "}")
         return "\n".join(result)
+
     return build(value, 0)
 
 
@@ -41,26 +54,30 @@ def plain_formatter(value):
         result = []
         for key, val in current_value.items():
             current_string = ""
-            if val.get("type") == "chained":
-                current_string = build(val["value"], create_path(current_path, key))
-            elif val.get("type") == "added":
-                if isinstance(val['value'], dict):
-                    current_string = f"Property '{create_path(current_path, key)}' was added with value: [complex value]"
-                else:
-                    current_string = f"Property '{create_path(current_path, key)}' was added with value: {format_exception_check(val['value'], 'single_quotes')}"
-            elif val.get("type") == "changed":
-                if isinstance(val['old_value'], dict):
-                    current_string = f"Property '{create_path(current_path, key)}' was updated. From [complex value] to {format_exception_check(val['new_value'], 'single_quotes')}"
-                elif isinstance(val['new_value'], dict):
-                    current_string = f"Property '{create_path(current_path, key)}' was updated. From {format_exception_check(val['old_value'], 'single_quotes')} to [complex value]"
-                else:
-                    current_string = f"Property '{create_path(current_path, key)}' was updated. From {format_exception_check(val['old_value'], 'single_quotes')} to {format_exception_check(val['new_value'], 'single_quotes')}"
-            elif val.get("type") == "deleted":
-                current_string = f"Property '{create_path(current_path, key)}' was removed"
-            else:
-                continue
+            match val.get("type"):
+                case "chained":
+                    current_string = build(val["value"], create_path(current_path, key))
+                case "added":
+                    if isinstance(val["value"], dict):
+                        current_string = f"Property '{create_path(current_path, key)}' was added with value: [complex value]"
+                    else:
+                        current_string = f"Property '{create_path(current_path, key)}' was added with value: {format_exception_check(val['value'], 'single_quotes')}"
+                case "changed":
+                    if isinstance(val["old_value"], dict):
+                        current_string = f"Property '{create_path(current_path, key)}' was updated. From [complex value] to {format_exception_check(val['new_value'], 'single_quotes')}"
+                    elif isinstance(val["new_value"], dict):
+                        current_string = f"Property '{create_path(current_path, key)}' was updated. From {format_exception_check(val['old_value'], 'single_quotes')} to [complex value]"
+                    else:
+                        current_string = f"Property '{create_path(current_path, key)}' was updated. From {format_exception_check(val['old_value'], 'single_quotes')} to {format_exception_check(val['new_value'], 'single_quotes')}"
+                case "deleted":
+                    current_string = (
+                        f"Property '{create_path(current_path, key)}' was removed"
+                    )
+                case _:
+                    continue
             result.append(current_string)
         return "\n".join(result)
+
     return build(value, "")
 
 
@@ -74,22 +91,22 @@ def json_formatter(value):
             return format_exception_check(current_value, "double_quotes")
 
         current_intend = replacer * depth
-        child_intend_size = depth + spaces_count
-        child_intend = replacer * child_intend_size
+        child_depth = depth + spaces_count
+        child_intend = replacer * child_depth
 
         children = ["{"]
         last_key = list(current_value.keys())[-1]
         for key, value in current_value.items():
-            if key != last_key:
-                if isinstance(value, dict):
-                    current_string = f'{child_intend}"{key}": {build(value, child_intend_size)},'
-                else:
-                    current_string = f'{child_intend}"{key}": {build(value, child_intend_size)},'
+            comma = "," if key != last_key else ""
+            if isinstance(value, dict):
+                current_string = (
+                    f'{child_intend}"{key}": {build(value, child_depth)}{comma}'
+                )
             else:
-                if isinstance(value, dict):
-                    current_string = f'{child_intend}"{key}": {build(value, child_intend_size)}'
-                else:
-                    current_string = f'{child_intend}"{key}": {build(value, child_intend_size)}'
+                current_string = (
+                    f'{child_intend}"{key}": {build(value, child_depth)}{comma}'
+                )
+
             children.append(current_string)
 
         children.append(current_intend + "}")
@@ -119,7 +136,7 @@ def format_exception_check(value, format="without_quotes"):
             return f"'{str(value)}'"
     elif format == "double_quotes":
         if isinstance(value, int):
-            return f'{str(value)}'
+            return f"{str(value)}"
         else:
             return f'"{str(value)}"'
     elif format == "without_quotes":
@@ -130,5 +147,5 @@ def main():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,22 +3,27 @@
 
 import argparse
 import json
+
 import yaml
-from gendiff.scripts.formatter import stylish_formatter
-from gendiff.scripts.formatter import plain_formatter
-from gendiff.scripts.formatter import json_formatter
+from gendiff.scripts.formatter import (
+    json_formatter,
+    plain_formatter,
+    stylish_formatter,
+)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares and shows a difference.")
+    parser = argparse.ArgumentParser(description="Shows differences.")
     parser.add_argument("first_file", type=str, help="First conf file")
     parser.add_argument("second_file", type=str, help="Second conf file")
     parser.add_argument(
-        "-f", "--format",
-        type=str, default="stylish",
+        "-f",
+        "--format",
+        type=str,
+        default="stylish",
         choices=["stylish", "json", "plain"],
-        help="set format of output (default: stylish)")
+        help="set format of output (default: stylish)",
+    )
 
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
@@ -26,6 +31,7 @@ def main():
 
 
 def generate_diff(first_file, second_file, format="stylish"):
+    print()
     node1 = read_file(first_file)
     node2 = read_file(second_file)
     diff = build_diff(node1, node2)
@@ -48,8 +54,7 @@ def build_diff(node1, node2):
         elif key not in node2:
             diff[key] = {"type": "deleted", "value": node1[key]}
         elif isinstance(node1[key], dict) and isinstance(node2[key], dict):
-            diff[key] = {
-                "type": "chained", "value": build_diff(node1[key], node2[key])}
+            diff[key] = {"type": "chained", "value": build_diff(node1[key], node2[key])}
         elif key in node1 and key in node2:
             diff[key] = evaluate_changes(node1, node2, key)
     return diff
@@ -57,23 +62,20 @@ def build_diff(node1, node2):
 
 def evaluate_changes(node1, node2, key):
     if node1[key] != node2[key]:
-        return {
-            "type": "changed",
-            "old_value": node1[key],
-            "new_value": node2[key]}
+        return {"type": "changed", "old_value": node1[key], "new_value": node2[key]}
     else:
         return {"type": "unchanged", "value": node1[key]}
 
 
 def read_file(path_to_file: str):
-    if '.yml' in path_to_file or '.yaml' in path_to_file:
+    if ".yml" in path_to_file or ".yaml" in path_to_file:
         with open(path_to_file) as file_to_parse:
             result = yaml.load(file_to_parse, Loader=yaml.FullLoader)
-    elif '.json' in path_to_file:
+    elif ".json" in path_to_file:
         with open(path_to_file) as file_to_parse:
             result = json.load(file_to_parse)
     else:
-        result = {'Exception': 'file has wrong format'}
+        result = {"Exception": "file has wrong format"}
     return result
 
 
