953486ba3edf00cb67e9cb5990003f3a550e0614
UsmanSakhbeev <sahbeyev@gmail.com>
2024-07-31 17:03:10 +0300

    rename files

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -1,39 +1,27 @@
+import pytest
 from gendiff.scripts.gendiff import generate_diff
 
 JSON1 = "tests/fixtures/json1.json"
 JSON2 = "tests/fixtures/json2.json"
 YML1 = "tests/fixtures/yml1.yml"
 YML2 = "tests/fixtures/yml2.yml"
-JSON_RESULT = "tests/fixtures/diff.json"
-PLAIN_RESULT = "tests/fixtures/plain_diff"
+STYLISH_RESULT = "tests/fixtures/stylish.txt"
+PLAIN_RESULT = "tests/fixtures/plain_diff.txt"
 YAML_RESULT = "tests/fixtures/json.json"
 
 
-def test_json_comparison():
-
-    with open(JSON_RESULT) as f:
+@pytest.mark.parametrize(
+    "file1, file2, format_name, result_file",
+    [
+        (JSON1, JSON2, "stylish", STYLISH_RESULT),
+        (JSON1, JSON2, "plain", PLAIN_RESULT),
+        (JSON1, JSON2, "json", YAML_RESULT),
+        (YML1, YML2, "stylish", STYLISH_RESULT),
+        (YML1, YML2, "plain", PLAIN_RESULT),
+        (YML1, YML2, "json", YAML_RESULT),
+    ],
+)
+def test_generate_diff(file1, file2, format_name, result_file):
+    with open(result_file) as f:
         expected_result = f.read().strip()
-    assert generate_diff(JSON1, JSON2, "stylish").strip() == expected_result
-
-    with open(PLAIN_RESULT) as f:
-        expected_result = f.read().strip()
-    assert generate_diff(JSON1, JSON2, "plain").strip() == expected_result
-
-    with open(YAML_RESULT) as f:
-        expected_result = f.read().strip()
-    assert generate_diff(JSON1, JSON2, "json").strip() == expected_result
-
-
-def test_yml_comparison():
-
-    with open(JSON_RESULT) as f:
-        expected_result = f.read().strip()
-    assert generate_diff(YML1, YML2, "stylish").strip() == expected_result
-
-    with open(PLAIN_RESULT) as f:
-        expected_result = f.read().strip()
-    assert generate_diff(YML1, YML2, "plain").strip() == expected_result
-
-    with open(YAML_RESULT) as f:
-        expected_result = f.read().strip()
-    assert generate_diff(YML1, YML2, "json").strip() == expected_result
+    assert generate_diff(file1, file2, format_name).strip() == expected_result
