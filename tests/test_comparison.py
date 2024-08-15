c2dd5d3dc786d52eb04143c79513e25d8fd7e948
UsmanSakhbeev <sahbeyev@gmail.com>
2024-08-15 13:48:31 +0300

    refactor test

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -2,48 +2,51 @@ import pytest
 from gendiff.scripts.gendiff import generate_diff
 
 
-@pytest.mark.parametrize(
-    "file1, file2, format_name, result_file",
-    [
-        (
-            "tests/fixtures/json1.json",
-            "tests/fixtures/json2.json",
-            "stylish",
-            "tests/fixtures/stylish.txt",
-        ),
-        (
-            "tests/fixtures/json1.json",
-            "tests/fixtures/json2.json",
-            "plain",
-            "tests/fixtures/plain_diff.txt",
-        ),
-        (
-            "tests/fixtures/json1.json",
-            "tests/fixtures/json2.json",
-            "json",
-            "tests/fixtures/json.json",
-        ),
-        (
-            "tests/fixtures/yml1.yml",
-            "tests/fixtures/yml2.yml",
-            "stylish",
-            "tests/fixtures/stylish.txt",
-        ),
-        (
-            "tests/fixtures/yml1.yml",
-            "tests/fixtures/yml2.yml",
-            "plain",
-            "tests/fixtures/plain_diff.txt",
-        ),
-        (
-            "tests/fixtures/yml1.yml",
-            "tests/fixtures/yml2.yml",
-            "json",
-            "tests/fixtures/json.json",
-        ),
-    ],
-)
-def test_generate_diff(file1, file2, format_name, result_file):
-    with open(result_file) as f:
+TEST_PARAMS = {
+    "json_stylish": {
+        "format": "stylish",
+        "files_to_compare": ["tests/fixtures/json1.json",
+                             "tests/fixtures/json2.json"],
+        "expected_output": "tests/fixtures/stylish.txt",
+    },
+    "json_plain": {
+        "format": "plain",
+        "files_to_compare": ["tests/fixtures/json1.json",
+                             "tests/fixtures/json2.json"],
+        "expected_output": "tests/fixtures/plain_diff.txt",
+    },
+    "json_yaml": {
+        "format": "json",
+        "files_to_compare": ["tests/fixtures/json1.json",
+                             "tests/fixtures/json2.json"],
+        "expected_output": "tests/fixtures/json.json",
+    },
+    "yml_stylish": {
+        "format": "stylish",
+        "files_to_compare": ["tests/fixtures/yml1.yml",
+                             "tests/fixtures/yml2.yml"],
+        "expected_output": "tests/fixtures/stylish.txt",
+    },
+    "yml_plain": {
+        "format": "plain",
+        "files_to_compare": ["tests/fixtures/yml1.yml",
+                             "tests/fixtures/yml2.yml"],
+        "expected_output": "tests/fixtures/plain_diff.txt",
+    },
+    "yml_yaml": {
+        "format": "json",
+        "files_to_compare": ["tests/fixtures/yml1.yml",
+                             "tests/fixtures/yml2.yml"],
+        "expected_output": "tests/fixtures/json.json",
+    },
+}
+
+
+@pytest.mark.parametrize("case", TEST_PARAMS.keys())
+def test_generate_diff(case):
+    params = TEST_PARAMS[case]
+    file1, file2 = params["files_to_compare"]
+    format_name = params["format"]
+    with open(params["expected_output"]) as f:
         expected_result = f.read().strip()
     assert generate_diff(file1, file2, format_name).strip() == expected_result
